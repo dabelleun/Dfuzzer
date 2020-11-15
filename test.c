b07eb014c3e25d3aefa94dc44081eacfd37906d4
dabelleun <73943365+dabelleun@users.noreply.github.com>
2020-11-15 13:49:20 +0900

    Update test.c

--- test.c
+++ test.c
@@ -28,8 +28,9 @@ int main(int argc, char* argv[]) //argc와 argv배열 포인터를 인자로하�
 		}
 		pclose(stream); //닫기
 		
+		int i;
 		printf("len : %d\n",strlen(buf)); //buf의 문자열 길이 출력
-		for(int i=0; i<strlen;(buf);i++); //
+		for(int i=0; i<strlen(buf);i++); //
 		{
 			printf("%2x",buf[i]);//buf[i]를 두자리 hexcode로 출력
 			if(buf[i] == '\x0a') //buf[i]가 줄바꿈되면
